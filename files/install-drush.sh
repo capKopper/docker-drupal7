b1f1894602bbf120f8e89cec42fc69d5ba431a24
Sébastien Kurtzemann <sebastien.k@capkopper.fr>
2015-02-20 15:48:23 +0100

    F Install drush binary only on the first container startup

--- files/install-drush.sh
+++ files/install-drush.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 user=$1
 
-echo "Installing drush for user '$user'..."
-mv /root/.composer /home/$user/ && chown -R $user:$user /home/$user/.composer
-ln -s /home/$user/.composer/vendor/bin/drush /usr/local/bin/
+if [ -d /root/.composer ]; then
+  echo "Installing drush for user '$user'..."
+  mv /root/.composer /home/$user/ && chown -R $user:$user /home/$user/.composer
+  ln -s /home/$user/.composer/vendor/bin/drush /usr/local/bin/
+fi
